b945db85bd7e3c32f04d3a04000a6494833e178f
Jonathan Pauluze <jonathanpauluze@gmail.com>
2025-07-05 20:56:55 -0300

    test(cart store): adding tryAddToCart tests

--- src/store/__tests__/cart.spec.ts
+++ src/store/__tests__/cart.spec.ts
@@ -2,15 +2,21 @@ import { createStore } from 'vuex'
 import cartModule from '@/store/modules/cart'
 import * as getPriceModule from '@/utils/getPrice'
 import { describe, it, expect, beforeEach, vi } from 'vitest'
+import type { Movie } from '@/types/movie'
+import type { RootState } from '../types'
+import type { ActionContext } from 'vuex/types/index.js'
 
-const mockMovie = {
+const mockMovie: Movie = {
   id: 101,
   title: 'Filme Teste',
+  original_title: 'Filme Teste',
   genre_ids: [1, 2],
   release_date: '2023-01-01',
   vote_average: 7.5,
   vote_count: 1000,
   poster_path: '/poster.jpg',
+  backdrop_path: '/poster.jpg',
+  adult: false,
 }
 
 vi.mock('@/utils/localStorage', () => ({
@@ -88,5 +94,83 @@ describe('cart Vuex module', () => {
 
     const count = store.getters['cart/cartCount']
     expect(count).toBe(2)
+  })
+
+  it('tryAddToCart dispatch mostra warning se o item já estiver no carrinho', () => {
+    const dispatch = vi.fn()
+    const commit = vi.fn()
+
+    const state = {
+      items: [mockMovie],
+    }
+
+    const context = {
+      state,
+      commit,
+      dispatch,
+      getters: {},
+      rootState: {} as RootState,
+      rootGetters: {},
+    } as ActionContext<typeof state, RootState>
+
+    cartModule.actions.tryAddToCart(context, mockMovie)
+
+    expect(commit).not.toHaveBeenCalled()
+    expect(dispatch).toHaveBeenCalledWith(
+      'toast/showToast',
+      expect.objectContaining({
+        message: `"${mockMovie.title}" já está no carrinho.`,
+        type: 'warning',
+      }),
+      { root: true },
+    )
+  })
+
+  it('tryAddToCart adiciona e mostra success se o item não estiver no carrinho', () => {
+    const dispatch = vi.fn()
+    const commit = vi.fn()
+
+    const state = {
+      items: [],
+    }
+
+    const context = {
+      state,
+      commit,
+      dispatch,
+      getters: {},
+      rootState: {} as RootState,
+      rootGetters: {},
+    } as ActionContext<typeof state, RootState>
+
+    cartModule.actions.tryAddToCart(context, mockMovie)
+
+    expect(commit).toHaveBeenCalledWith('addToCart', mockMovie)
+    expect(dispatch).toHaveBeenCalledWith(
+      'toast/showToast',
+      expect.objectContaining({
+        message: `"${mockMovie.title}" adicionado ao carrinho!`,
+        type: 'success',
+      }),
+      { root: true },
+    )
+  })
+
+  it('isInCart retorna true se o item estiver no carrinho', () => {
+    const state = {
+      items: [mockMovie],
+    }
+
+    const result = cartModule.getters.isInCart(state)(mockMovie.id)
+    expect(result).toBe(true)
+  })
+
+  it('isInCart retorna false se o item NÃO estiver no carrinho', () => {
+    const state = {
+      items: [],
+    }
+
+    const result = cartModule.getters.isInCart(state)(mockMovie.id)
+    expect(result).toBe(false)
   })
 })
